fc13fac7a94333073d2d18f5fa7990d9165398e5
Willi <13342175+willi-nuechterlein@users.noreply.github.com>
2023-04-30 00:58:58 +0200

    add post mocks

---
+++ src/lib/mocks/posts.ts
@@ -0,0 +1,122 @@
+export const POSTS = [
+  {
+    id: 1,
+    title: 'Post 1',
+    shortDescription: 'Short description 1',
+    tags: ['tag1', 'tag2'],
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    voluptatum, voluptate, quibusdam, quia voluptas quod quos
+    voluptatibus quae doloribus quas natus. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum`,
+    createdAt: new Date()
+  },
+  {
+    id: 2,
+    title: 'Post 2',
+    shortDescription: 'Short description 2',
+    tags: ['tag1', 'tag2'],
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    voluptatum, voluptate, quibusdam, quia voluptas quod quos
+    voluptatibus quae doloribus quas natus. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum`,
+    createdAt: new Date()
+  },
+  {
+    id: 3,
+    title: 'Post 3',
+    shortDescription: 'Short description 3',
+    tags: ['tag1', 'tag2'],
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    voluptatum, voluptate, quibusdam, quia voluptas quod quos
+    voluptatibus quae doloribus quas natus. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum`,
+    createdAt: new Date()
+  },
+  {
+    id: 4,
+    title: 'Post 4',
+    shortDescription: 'Short description 4',
+    tags: ['tag1', 'tag2'],
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    voluptatum, voluptate, quibusdam, quia voluptas quod quos
+    voluptatibus quae doloribus quas natus. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum`,
+    createdAt: new Date()
+  },
+  {
+    id: 5,
+    title: 'Post 5',
+    shortDescription: 'Short description 5',
+    tags: ['tag1', 'tag2'],
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    voluptatum, voluptate, quibusdam, quia voluptas quod quos
+    voluptatibus quae doloribus quas natus. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum`,
+    createdAt: new Date()
+  },
+  {
+    id: 6,
+    title: 'Post 6',
+    shortDescription: 'Short description 6',
+    tags: ['tag1', 'tag2'],
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    voluptatum, voluptate, quibusdam, quia voluptas quod quos
+    voluptatibus quae doloribus quas natus. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum`,
+    createdAt: new Date()
+  },
+  {
+    id: 7,
+    title: 'Post 7',
+    shortDescription: 'Short description 7',
+    tags: ['tag1', 'tag2'],
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    voluptatum, voluptate, quibusdam, quia voluptas quod quos
+    voluptatibus quae doloribus quas natus. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum`,
+    createdAt: new Date()
+  },
+  {
+    id: 8,
+    title: 'Post 8',
+    shortDescription: 'Short description 8',
+    tags: ['tag1', 'tag2'],
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    voluptatum, voluptate, quibusdam, quia voluptas quod quos
+    voluptatibus quae doloribus quas natus. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum`,
+    createdAt: new Date()
+  },
+  {
+    id: 9,
+    title: 'Post 9',
+    shortDescription: 'Short description 9',
+    tags: ['tag1', 'tag2'],
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    voluptatum, voluptate, quibusdam, quia voluptas quod quos
+    voluptatibus quae doloribus quas natus. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum`,
+    createdAt: new Date()
+  },
+  {
+    id: 10,
+    title: 'Post 10',
+    shortDescription: 'Short description 10',
+    tags: ['tag1', 'tag2'],
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    voluptatum, voluptate, quibusdam, quia voluptas quod quos
+    voluptatibus quae doloribus quas natus. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
+    voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum`,
+    createdAt: new Date()
+  }
+]
