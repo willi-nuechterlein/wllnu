f7d4996101cba90bf0c3ecfa5f98f479ba84224c
Willi <13342175+willi-nuechterlein@users.noreply.github.com>
2023-04-30 00:59:35 +0200

    render posts in grid cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { POSTS } from '@/lib/mocks/posts'
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <>
@@ -6,19 +9,25 @@ export default function Home() {
       </header>
       <main>
         <section className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-          {Array.from({
-            length: 20
-          }).map((_, i) => (
-            <article
-              key={i}
-              className="p-4 border rounded-lg border-slate-300 hover:cursor-pointer hover:border-slate-400"
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptatum, voluptate, quibusdam, quia voluptas quod quos
-              voluptatibus quae doloribus quas natus. Quisquam, voluptatum
-              voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
-              voluptate. Quisquam, voluptatum voluptate. Quisquam, voluptatum
-            </article>
+          {POSTS.map(({ id, title, shortDescription, tags }) => (
+            <Link href={`/post/${id}`} key={id}>
+              <article className="p-4 border rounded-lg border-slate-300 hover:cursor-pointer hover:border-slate-400 hover:scale-[1.01]">
+                <h2 className="mb-2 text-lg font-medium">{title}</h2>
+                <p className="mb-2 text-sm text-slate-500">
+                  {shortDescription}
+                </p>
+                <ul className="flex flex-wrap">
+                  {tags.map((tag) => (
+                    <li
+                      key={tag}
+                      className="px-2 py-1 mb-2 mr-2 text-xs border rounded-lg text-slate-500 border-slate-400"
+                    >
+                      {tag}
+                    </li>
+                  ))}
+                </ul>
+              </article>
+            </Link>
           ))}
         </section>
       </main>
