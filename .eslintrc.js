110fda8d251789094e440a97d06ba6f37016e811
Willi <13342175+willi-nuechterlein@users.noreply.github.com>
2023-04-29 23:54:32 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:jest/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+    'react',
+    '@typescript-eslint'
+    // 'jest'
+  ],
+  env: {
+    browser: true,
+    es6: true
+    // jest: true
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'linebreak-style': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ]
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "endOfLine": "lf",
+    "printWidth": 80,
+    "semi": false,
+    "singleQuote": true,
+    "quoteProps": "preserve",
+    "tabWidth": 2,
+    "trailingComma": "none",
+    "useTabs": false
+  }
