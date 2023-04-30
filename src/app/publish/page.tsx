a1b77c8f09d80ae70a4b61ac9dba339fad72fe4f
Willi <13342175+willi-nuechterlein@users.noreply.github.com>
2023-04-30 20:44:32 +0200

    add missing form fields

--- src/app/publish/page.tsx
+++ src/app/publish/page.tsx
@@ -7,7 +7,7 @@ import Button from '@/components/atoms/Button'
 interface PostForm {
   title: string
   description: string
-  tags: Array<string>
+  tags: string
   content: string
 }
 
@@ -15,7 +15,7 @@ export default async function PublishPage() {
   const handleSubmit = async (values: PostForm) => {
     const response = await fetch('/api/post', {
       method: 'POST',
-      body: JSON.stringify({ ...values, tags: [values.tags] })
+      body: JSON.stringify({ ...values, tags: values.tags.split(',') })
     })
     const result = await response.json()
     console.log(result)
@@ -63,6 +63,95 @@ export default async function PublishPage() {
               )
             }}
           </Field>
+          <Field<PostForm['description']>
+            name="description"
+            onBlurValidate={z
+              .string()
+              .min(5, 'Must be at least 5 characters long')}
+          >
+            {({ value, setValue, onBlur, errors }) => {
+              return (
+                <div className="flex flex-col gap-1 ">
+                  <label
+                    htmlFor="description"
+                    className="text-sm text-gray-600 "
+                  >
+                    Description
+                  </label>
+                  <input
+                    className="p-1.5 border border-gray-300 rounded-md"
+                    value={value}
+                    onBlur={onBlur}
+                    onChange={(e) => setValue(e.target.value)}
+                    placeholder={'Description'}
+                  />
+                  {errors.map((error) => (
+                    <p key={error} className="text-sm text-red-500">
+                      {error}
+                    </p>
+                  ))}
+                </div>
+              )
+            }}
+          </Field>
+          <Field<PostForm['tags']>
+            name="tags"
+            onBlurValidate={z
+              .string()
+              .min(5, 'Must be at least 5 characters long')}
+          >
+            {({ value, setValue, onBlur, errors }) => {
+              return (
+                <div className="flex flex-col gap-1 ">
+                  <label htmlFor="tags" className="text-sm text-gray-600 ">
+                    Tags
+                  </label>
+                  <input
+                    className="p-1.5 border border-gray-300 rounded-md"
+                    value={value}
+                    onBlur={onBlur}
+                    onChange={(e) => setValue(e.target.value)}
+                    placeholder={'Tags'}
+                  />
+                  {errors.map((error) => (
+                    <p key={error} className="text-sm text-red-500">
+                      {error}
+                    </p>
+                  ))}
+                </div>
+              )
+            }}
+          </Field>
+          <Field<PostForm['content']>
+            name="content"
+            onBlurValidate={z
+              .string()
+              .min(5, 'Must be at least 5 characters long')}
+          >
+            {({ value, setValue, onBlur, errors }) => {
+              return (
+                <div className="flex flex-col gap-1 ">
+                  <label htmlFor="content" className="text-sm text-gray-600 ">
+                    Content
+                  </label>
+                  <textarea
+                    rows={10}
+                    className="p-1.5 border border-gray-300 rounded-md"
+                    value={value}
+                    onBlur={onBlur}
+                    onChange={(e) => setValue(e.target.value)}
+                    placeholder={'Content'}
+                  />
+                  {errors.map((error) => (
+                    <p key={error} className="text-sm text-red-500">
+                      {error}
+                    </p>
+                  ))}
+                </div>
+              )
+            }}
+          </Field>
+
           <Button type="submit">Publish</Button>
         </form>
       )}
