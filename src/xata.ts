397034355765c23748d5e49471196319d1eee7c4
Willi <13342175+willi-nuechterlein@users.noreply.github.com>
2023-04-30 16:27:35 +0200

    add xata

---
+++ src/xata.ts
@@ -0,0 +1,55 @@
+import { buildClient } from '@xata.io/client'
+import type {
+  BaseClientOptions,
+  SchemaInference,
+  XataRecord
+} from '@xata.io/client'
+
+const tables = [
+  {
+    name: 'posts',
+    columns: [
+      { name: 'title', type: 'string', notNull: true, defaultValue: 'null' },
+      {
+        name: 'description',
+        type: 'string',
+        notNull: true,
+        defaultValue: 'null'
+      },
+      { name: 'tags', type: 'multiple' },
+      { name: 'content', type: 'text' },
+      { name: 'embedding', type: 'vector', vector: { dimension: 1536 } }
+    ]
+  }
+] as const
+
+export type SchemaTables = typeof tables
+export type InferredTypes = SchemaInference<SchemaTables>
+
+export type Posts = InferredTypes['posts']
+export type PostsRecord = Posts & XataRecord
+
+export type DatabaseSchema = {
+  posts: PostsRecord
+}
+
+const DatabaseClient = buildClient()
+
+const defaultOptions = {
+  databaseURL: process.env.XATA_DATABASE_URL || ''
+}
+
+export class XataClient extends DatabaseClient<DatabaseSchema> {
+  constructor(options?: BaseClientOptions) {
+    super({ ...defaultOptions, ...options }, tables)
+  }
+}
+
+let instance: XataClient | undefined = undefined
+
+export const getXataClient = () => {
+  if (instance) return instance
+
+  instance = new XataClient()
+  return instance
+}
