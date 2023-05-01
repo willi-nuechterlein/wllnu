4fee3f0e4207afd01d5d4138897beb848808b7d1
Willi <13342175+willi-nuechterlein@users.noreply.github.com>
2023-05-01 22:24:08 +0200

    add WIP label

---
+++ src/components/atoms/UnderConstruction/index.tsx
@@ -0,0 +1,10 @@
+const UnderConstruction = () => {
+  return (
+    <p className="fixed w-64 p-6 py-4 text-sm text-amber-100 bg-amber-600 rounded-xl bottom-4 left-4">
+      <span className="block text-base font-semibold">WIP</span>
+      content displayed on this page is placeholder dummy content
+    </p>
+  )
+}
+
+export default UnderConstruction
